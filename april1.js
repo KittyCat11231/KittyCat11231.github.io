345e216b96adaa6fb25f30f360f6533b005931ac
KittyCat11231 <kittycat61500@gmail.com>
2024-03-31 23:11:26 -0400

    April 1, 2024

---
+++ april1.js
@@ -0,0 +1,13 @@
+function loop() {
+    while (true) {
+        console.log('The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.');
+        console.log('Peter Piper picked a peck of pickle peppers. If Peter Piper picked a peck of pickle peppers, how many pickle peppers did Peter Piper pick?');
+        console.log('Somebody once told me the world is gonna roll me. I ain\'t the sharpest tool in the shed.');
+        console.log('My anaconda don\'t. My anaconda don\'t. My anaconda don\'t want none unless you got buns hun.');
+        console.log('The Not A Van is inside the STC bus storage depot across the street from the Whiteley Airport Parkway RaiLinQ station.');
+    }
+}
+
+document.getElementById("loop").addEventListener('click', function () {
+    loop();
+})
